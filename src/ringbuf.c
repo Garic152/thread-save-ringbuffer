b9161271bdf3dc0f4dbdb4971021fabf8bf54a2c
Garic152 <erictheiler15@gmail.com>
2024-07-04 20:01:37 +0200

    basic read and firewall functionality

--- src/ringbuf.c
+++ src/ringbuf.c
@@ -7,6 +7,37 @@
 
 #define TIMEOUT 1
 
+uint8_t *write_inc(rbctx_t *context, uint8_t *writer_ptr) {
+  writer_ptr += 1;
+  if (writer_ptr >= context->end) {
+    writer_ptr = context->begin;
+  }
+  return writer_ptr;
+}
+
+void read_inc(rbctx_t *context) {
+  context->read += 1;
+  if (context->read >= context->end) {
+    context->read = context->begin;
+  }
+}
+
+size_t write_space(rbctx_t *context) {
+  if (context->write >= context->read) {
+    return context->end - context->write + context->read - context->begin - 1;
+  } else {
+    return context->read - context->write - 1;
+  }
+}
+
+size_t read_space(rbctx_t *context) {
+  if (context->write >= context->read) {
+    return context->write - context->read;
+  } else {
+    return context->end - context->read + context->write - context->begin;
+  }
+}
+
 void ringbuffer_init(rbctx_t *context, void *buffer_location,
                      size_t buffer_size) {
   context->begin = buffer_location;
@@ -20,67 +51,33 @@ void ringbuffer_init(rbctx_t *context, void *buffer_location,
   pthread_cond_init(&context->signal_write, NULL);
 }
 
-size_t write_space(rbctx_t *context) {
-  size_t space;
-
-  if (context->write == context->read) {
-    space = context->end - context->begin - 1;
-  } else {
-    space = (context->write > context->read)
-                ? (context->end - context->write + context->read -
-                   context->begin - 1)
-                : (context->read - context->write - 1);
-  }
-  return space;
-}
-
-size_t read_space(rbctx_t *context) {
-  size_t space;
-  if (context->read == context->write) {
-    space = 0;
-  } else {
-    space =
-        (context->read > context->write)
-            ? (context->end - context->read + context->write - context->begin)
-            : (context->write - context->read);
-  }
-  return space;
-}
-
 int ringbuffer_write(rbctx_t *context, void *message, size_t message_len) {
   pthread_mutex_lock(&context->mutex_write);
 
-  struct timespec ts;
-  clock_gettime(CLOCK_REALTIME, &ts);
-  ts.tv_sec += TIMEOUT;
-  int rt = 0;
-
   while (write_space(context) < message_len + sizeof(size_t)) {
-    rt = pthread_cond_timedwait(&context->signal_write, &context->mutex_write,
-                                &ts);
-    if (rt != 0) {
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    ts.tv_sec += TIMEOUT;
+
+    if (pthread_cond_timedwait(&context->signal_write, &context->mutex_write,
+                               &ts) != 0) {
       pthread_mutex_unlock(&context->mutex_write);
       return RINGBUFFER_FULL;
     }
   }
 
-  for (size_t i = 0; i < sizeof(size_t); i++) {
-    // *context->write = (uint8_t)((message_len >> (8 * i)) & 0xFF);
-    *(uint8_t *)(context->write) = (uint8_t)((message_len >> (8 * i)) & 0xFF);
-    context->write += 1;
-    if (context->write >= context->end) {
-      context->write = context->begin;
-    }
+  uint8_t *write = context->write;
+  for (size_t i = 0; i < sizeof(message_len); i++) {
+    *write = (uint8_t)((message_len >> (8 * i)) & 0xFF);
+    write = write_inc(context, write);
   }
 
-  for (size_t i = 0; i < message_len; i++) {
-    *context->write = ((char *)message)[i];
-    context->write += 1;
-
-    if (context->write >= context->end) {
-      context->write = context->begin;
-    }
+  for (int i = 0; i < message_len; i++) {
+    *write = ((char *)message)[i];
+    write = write_inc(context, write);
   }
+
+  context->write = write;
 
   pthread_cond_signal(&context->signal_read);
   pthread_mutex_unlock(&context->mutex_write);
@@ -97,42 +94,31 @@ int ringbuffer_read(rbctx_t *context, void *buffer, size_t *buffer_len) {
 
   size_t message_len = 0;
   for (size_t i = 0; i < sizeof(size_t); i++) {
-    // message_len |= (uint8_t)*context->read << (8 * i);
-    message_len |= ((size_t)(*(uint8_t *)(context->read))) << (8 * i);
-    context->read += 1;
-    if (context->read >= context->end) {
-      context->read = context->begin;
-    }
+    message_len |= (uint8_t)*context->read << (8 * i);
+    read_inc(context);
   }
 
   if (message_len > *buffer_len) {
     pthread_mutex_unlock(&context->mutex_read);
-    printf("HELP");
     return OUTPUT_BUFFER_TOO_SMALL;
   }
 
-  struct timespec ts;
-  clock_gettime(CLOCK_REALTIME, &ts);
-  ts.tv_sec += TIMEOUT;
-  int rt = 0;
-
   while (read_space(context) < message_len) {
-    rt = pthread_cond_timedwait(&context->signal_read, &context->mutex_read,
-                                &ts);
-    if (rt != 0) {
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    ts.tv_sec += TIMEOUT;
+
+    if (pthread_cond_timedwait(&context->signal_read, &context->mutex_read,
+                               &ts) != 0) {
       pthread_mutex_unlock(&context->mutex_read);
-      return RBUF_TIMEOUT;
+      return RINGBUFFER_EMPTY;
     }
   }
 
   *buffer_len = message_len;
   for (size_t i = 0; i < message_len; i++) {
     ((char *)buffer)[i] = *context->read;
-    context->read += 1;
-
-    if (context->read >= context->end) {
-      context->read = context->begin;
-    }
+    read_inc(context);
   }
 
   pthread_cond_signal(&context->signal_write);
@@ -142,7 +128,7 @@ int ringbuffer_read(rbctx_t *context, void *buffer, size_t *buffer_len) {
 
 void ringbuffer_destroy(rbctx_t *context) {
   pthread_mutex_destroy(&context->mutex_write);
-  pthread_mutex_destroy(&context->mutex_write);
+  pthread_mutex_destroy(&context->mutex_read);
   pthread_cond_destroy(&context->signal_read);
   pthread_cond_destroy(&context->signal_write);
 }
